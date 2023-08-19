b7d8d96a50e42b04e7944b52ef5fc842aa839583
Zarul Zubir <zarul.izham@gmail.com>
2023-08-20 05:50:22 +0800

    Fix encode ArgumentCountError

--- src/Support/Authentication/JWT.php
+++ src/Support/Authentication/JWT.php
@@ -8,7 +8,7 @@ class JWT
 {
     public static function generateToken($token, $secret)
     {
-        return FirebaseJWT::encode($token, $secret);
+        return FirebaseJWT::encode($token, $secret, 'HS256');
     }
 
     public static function decodeToken($jwt, $secret)
